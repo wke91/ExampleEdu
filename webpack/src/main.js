28a18f2ab5fefb74ef35ddb9f8fb9953f1b85dce
wangkeb <wangkeb@ufsoft.com.cn>
2017-02-27 13:35:08 +0800

    培训例子2

--- webpack/src/main.js
+++
@@ -1,7 +0,0 @@
-// main.js
-var greeter = require('./Greeter.js');
-var fill0 = require('./test.js');
-
-document.getElementById('root').appendChild(greeter());
-
-document.getElementById('test').innerHTML = fill0(7);
--- webpack/src/test.js
+++
@@ -1,5 +0,0 @@
-// test.js
-module.exports = function (num) {
-	"use strict";
-	return num < 10 ? '0' + num : num;
-};
